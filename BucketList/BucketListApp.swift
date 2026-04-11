af26c4b7bdd8baf242e54dbbdb8a16b18c3ed9c7
Sergey Zakharov <zakharov.nsg@gmail.com>
2026-04-12 02:51:57 +0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Сергей Захаров on 12.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
